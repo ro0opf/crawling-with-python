93d4d96d09299919b3d64d66f951c0681bc9759f
ro0opf <34717634+ro0opf@users.noreply.github.com>
2021-01-20 01:20:36 +0900

    ,,,,

--- heedo jeon/googlemaps-with-marker/src/pages/MainPage.tsx
+++ heedo jeon/googlemaps-with-marker/src/pages/MainPage.tsx
@@ -1,10 +1,38 @@
 // src/pages/MainPage.tsx
 import React from 'react'
+import GoogleMapReact from 'google-map-react';
+
+const Marker = (props: any) => {
+  const { color, name, id } = props;
+  return (
+    <div className="marker"
+      style={{ backgroundColor: color, cursor: 'pointer'}}
+      title={name}>
+        {name}
+      </div>
+  );
+};
 
 function MainPage() {
+  let defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
+
   return (
     <>
-      <div>asefasefasf</div>
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyCT5wjfqFRNzYHN-yB4iavQdd5kHRfckWQ" }}
+          defaultCenter={defaultProps.center}
+          defaultZoom={defaultProps.zoom}
+        >
+          <Marker lat={59.95} lng={30.33} name={"awdaw"} />
+        </GoogleMapReact>
+      </div>
     </>
   )
 }
